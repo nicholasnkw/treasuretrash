c7d176b384c085ae21b15529e4215124d51927ba
Nicholas <nicholasnkw@gmail.com>
2016-10-07 02:14:32 +0800

    basic transaction mvc

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'home#index'
   resources :postings do 
   	resources :comments
+    resources :transactions
   end
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161006103235) do
+ActiveRecord::Schema.define(version: 20161006165911) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,16 @@ ActiveRecord::Schema.define(version: 20161006103235) do
     t.json     "avatars"
   end
 
+  create_table "transactions", force: :cascade do |t|
+    t.integer  "posting_id"
+    t.boolean  "status"
+    t.boolean  "success"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "transactions", ["posting_id"], name: "index_transactions_on_posting_id", using: :btree
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -65,4 +75,5 @@ ActiveRecord::Schema.define(version: 20161006103235) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "transactions", "postings"
 end
