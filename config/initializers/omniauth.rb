6a119f5e82e75dec25ec7dba199f6d687427a2dd
Nurul Hanisah Kamarul Zaman <HanisahKZ@Nuruls-MacBook-Air.local>
2016-10-07 14:38:49 +0800

    add navbar

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
-
-# Rails.application.config.middleware.use OmniAuth::Builder do 
-# 	provider :facebook, ENV['facebook_app_id'], ENV['facebook_app_secret']
+# Rails.application.config.middleware.use OmniAuth::Builder do
+#   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
+#   scope: 'email, image', display: 'popup'
 # end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161007034133) do
+ActiveRecord::Schema.define(version: 20161007045804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 20161007034133) do
     t.string   "uid"
     t.string   "avatar"
     t.string   "identicon"
+    t.string   "facebook_image"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
