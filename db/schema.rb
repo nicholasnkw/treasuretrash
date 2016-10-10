6a20d2cf07a0a280f596fe638ea1647571ecfcc4
Nicholas <nicholasnkw@gmail.com>
2016-10-10 12:06:17 +0800

    added credit column to User table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161007083427) do
+ActiveRecord::Schema.define(version: 20161010040157) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,9 +34,9 @@ ActiveRecord::Schema.define(version: 20161007083427) do
     t.string   "city"
     t.string   "state"
     t.string   "zipcode"
-    t.boolean  "availability"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.boolean  "availability", default: true
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
     t.float    "latitude"
     t.float    "longitude"
     t.json     "avatars"
@@ -58,12 +58,12 @@ ActiveRecord::Schema.define(version: 20161007083427) do
   add_index "transactions", ["user_id"], name: "index_transactions_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",  null: false
+    t.string   "encrypted_password",     default: "",  null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,   null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -71,13 +71,14 @@ ActiveRecord::Schema.define(version: 20161007083427) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "phone_no"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
     t.string   "provider"
     t.string   "uid"
     t.string   "avatar"
     t.string   "identicon"
     t.string   "facebook_image"
+    t.integer  "credit",                 default: 100
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
